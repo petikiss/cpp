5fce25ed6dcfd6324b1d2176f5d579d4176ae839
Vacak Work <work@vacak.hu>
2016-03-04 11:29:18 +0100

    factory

---
+++ small_snippets/2015/head/4_factory/main5.cpp
@@ -0,0 +1,78 @@
+// Very depentent PizzaStore
+
+
+#include <string>
+#include <iostream>
+
+
+class Pizza
+{
+  public:
+   void prepare() {}
+   void bake() {}
+   void cut() {}
+  private:
+};
+
+
+class ChichagoStyleGreekPizza : public Pizza
+{
+};
+
+class ChichagoStyleCheesePizza : public Pizza
+{
+};
+
+class NyStyleCheesePizza : public Pizza
+{
+};
+
+class NyStyleGreekPizza : public Pizza
+{
+};
+
+
+class DepententPizzaShop
+{
+  public:
+
+    // This "Creator" creates all concrete objects instead of delegating to a factory
+    Pizza* createPizza(std::string style, std::string type)
+    {
+        Pizza* pizza = NULL;
+
+        if (style == "Chichago")
+        {
+            if (type == "cheese")
+            {
+                // if ChichagoStyleCheesePizza  modified
+                // we may  modify the DepententPizzaShop ...
+                // reducing depending to concrete classis is "good" -> formal name: DIP
+                pizza = new ChichagoStyleCheesePizza();
+            }
+            else if (type == "greek")
+            {
+                pizza = new ChichagoStyleGreekPizza();
+            }
+        }
+        else if (style == "NY")
+        {
+            if (type == "cheese")
+            {
+                pizza = new NyStyleCheesePizza();
+            }
+            else if (type == "greek")
+            {
+                pizza = new NyStyleGreekPizza();
+            }
+        }
+        return pizza;
+    }
+};
+
+
+
+
+int main()
+{
+}
