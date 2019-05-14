f81693d2478f7c684d9043cb05405a58a4c8c184
vacak <you@example.com>
2019-05-14 09:57:54 +0200

    reverse polish notation

---
+++ algorithm/reversepolishnotation/main.cpp
@@ -0,0 +1,54 @@
+#include <string>
+#include <stack>
+
+
+
+class Tokenizer
+{
+  public:
+   Tokenizer(std::string input) : mInput(input), mCurrentPosition(0) {}
+   std::string getNextToken()
+      {
+
+      }
+
+  private:
+   std::string mInput;
+   int mCurrentPosition;
+};
+
+
+
+
+std::string searchToken(const std::string& input)
+{
+
+}
+
+
+
+
+
+
+int main()
+{
+   std::string input = "(icsi-ref=icsi.mmtel||mobility=fixed)&&marci!=true";
+
+   std::stack<std::string> stack;
+
+
+
+
+/*
+   if (currentChar == '(')
+   {
+      stack.push("(");
+   }
+   else if(true)
+   {
+   }
+*/
+
+
+   } // for
+}
